67f0ee40610405456ece42434c6ce28f29ae6cda
Arjesh <arjesh.khadka2@gmail.com>
2023-03-09 09:13:41 +1100

    installation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      helolo
+      <Button>hello</Button>
+    </div>
+  );
+}
+
+export default App;
